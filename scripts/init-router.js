a39d12f7c3859d15591d4c07ad283bb41018be6f
root <root@localhost.localdomain>
2019-02-15 03:04:30 -0500

    ajust service

--- scripts/init-router.js
+++ scripts/init-router.js
@@ -6,9 +6,6 @@ sh.addShard("shard01/shard01b:27018")
 sh.addShard("shard02/shard02a:27019")
 sh.addShard("shard02/shard02b:27019")
 
-sh.addShard("shard03/shard03a:27020")
-sh.addShard("shard03/shard03b:27020")
-
 db.runCommand({ enablesharding:"test" })
 db.runCommand({ shardcollection:"test.users", key:{ _id:1 } })
 
@@ -16,6 +13,6 @@ use config
 db.settings.save( { _id:"chunksize", value: 1 } )
 
 use test
-for (var i = 1; i < 55555; i++) {
-	db.users.insert( { id:i,name:"name"+i } );
+for (var i = 1; i < 22222; i++) {
+	db.users.insert( { id:i+"_"+Math.random().toString(12).substr(2),name:Math.random().toString(36).substr(2) } );
 }
